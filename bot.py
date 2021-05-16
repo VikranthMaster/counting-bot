874b04be9c4d279c619c21af280e5a3a2c17139f
VikranthMaster <69100217+VikranthMaster@users.noreply.github.com>
2021-05-16 08:56:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,10 @@ import sys
 import os.path
 from PIL import Image
 import asyncio
+import time
 
 client = commands.Bot(command_prefix=".")
+client.remove_command('help')
 
 class BotData:
     def __init__(self):
@@ -22,91 +24,96 @@ client1 = discord.Client()
 
 botdata = BotData()
 
+
 @client.event
 async def on_ready():
     print(f"{client.user} ready!")
 
-@client.command(aliases=["poke","pokemon","p","about"])
-async def find(ctx,*,pokemon_name):
-    pokemon = pypokedex.get(name=pokemon_name)
-    http = urllib3.PoolManager()
-    response = http.request('GET', pokemon.sprites.front.get('default'))
-    image = PIL.Image.open(BytesIO(response.data))
-    img = image.save("pokemon.png")
-    await ctx.send(f"{pokemon_name} found!")
-    with open("pokemon.png","rb") as fp:
-        print("All is working good")
 
-    type_pokemon = pokemon.types
-    res = str(type_pokemon)[1:-1]
-
-    filePath = discord.File("C:/Users/shankar/OneDrive/Desktop/shanmukha/Python/Discord/pokemon.png", filename = "pokemon.png")
-
-    embed = discord.Embed(color = discord.Color.blue())
-    embed.add_field(name = f"**__{pokemon.name}__**", value = f"Showing Stats for {pokemon.name}", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Pokedex number**: ``{pokemon.dex}``", inline=False)
-    embed.add_field(name = f"\u200b", value = f"**Types**: ``{res}``",inline=False)
-    embed.add_field(name = f"\u200b",value = f"**HP**: ``{pokemon.base_stats.hp}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Attack**: ``{pokemon.base_stats.attack}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Defence**: ``{pokemon.base_stats.defense}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Special Attack**: ``{pokemon.base_stats.sp_atk}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Special Defence**: ``{pokemon.base_stats.sp_def}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Height**: ``{pokemon.height*10}``", inline=False)
-    embed.add_field(name = f"\u200b",value = f"**Weight**: ``{pokemon.weight/10}``", inline=False)
-
-    embed.set_image(url = "attachment://pokemon.png")
-    embed.set_thumbnail(url = ctx.author.avatar_url)
-    await ctx.send(file = filePath,embed=embed)
-
-@client.command(aliases=["counting"])
+@client.command(aliases=["counting", "set_counting_channel"])
 async def counting_channel(ctx, channel: discord.TextChannel):
-    if channel != None:
-        if channel.name == channel:
-            botdata.counting_channel = channel
-            await ctx.channel.send(f"Counting channel has been set to: {channel.name}")
-            await channel.send(f"This is your counting channel")
-
+    if channel == None:
+        await ctx.send("The channel you have provided is incorrect")
     else:
-        await ctx.channel.send("You didnt configure a counting channel")
+        botdata.counting_channel = channel
+        embed = discord.Embed(color = discord.Color.blue())
+        embed.add_field(name = f"**__Welcome to Counting Bot__ :robot:**", value = f"So you have choosen to start your counting in {channel} good luck !! :partying_face:", inline = False)
+        embed.add_field(name = f"**__Rules__**", value = f"Rules are pretty simple\n **``1)Do not break the chain\n2)The message u send should be an integer\n3)One person cant send two messages at a time\n``**")
+        embed.add_field(name = f"**__Disclaimer__ :octagonal_sign:**", value = f"If you break any one of the rule, you and your server memebers have to start the game from the beginning so be mindful of what are about to send")
+        embed.set_footer(text="Remember you can always change the channel")
+        await ctx.send(embed=embed)
+
+        embed = discord.Embed(color = discord.Color.blue())
+        embed.add_field(name = f"**Channel**", value = f"This is your counting channel...**WAIT BEFORE THIS MESSAGE DELETES and** **Start the count from ``1``**",inline=False)
+        embed.set_footer(text=" :partying_face: Good luck !")
+        something = await channel.send(embed=embed)
+        await asyncio.sleep(10)
+        await something.delete()
+        # print(botdata.counting_channel.id)
 
 
 @client.listen()
 async def on_message(message):
     async def clear(ctx, amount):
         await ctx.channel.purge(limit = amount + 1)
-    
-    channel = client.get_channel(842990700308070420)
+
+    channel = client.get_channel(botdata.counting_channel.id)
+
     if message.channel != channel or message.author.bot:
         #wrong channel or author is a bot
         return
 
+    history = await channel.history().flatten()
     latest_messages = await channel.history(limit=2).flatten()
     lastest_message2 = latest_messages[1].author
     lastest_message1 = latest_messages[0].author
     if not all(msg.content.isdigit() for msg in latest_messages):
         #not all the messages are an int
         return
-    
+
     if not message.content.isdigit():
         #input message is not an int
-        await clear(message,int(latest_messages[0].content))
+        embed = discord.Embed(color= discord.Color.blue())
+        embed.add_field(name=f"**Message not a integer :x:**", value = f":bangbang: {lastest_message1.mention} Dude it should be a number not a integer :bangbang:")
+        await channel.send(embed=embed)
+        await asyncio.sleep(20)
+        await channel.purge(limit=1)
+        await clear(message,100000000)
 
-    if (int(latest_messages[0].content) - int(latest_messages[1].content)) == 1 and lastest_message1 != lastest_message2:
-        print("looking good")
-    else:   
-        print("Number squence break")
-        await clear(message,int(latest_messages[0].content))
-    
-    # history = await channel.history().flatten()
-    # guild = message.guild
-    # if lastest_message1 == lastest_message2:
-    #     print("User repeated")
-    #     await clear(message,int(latest_messages[0].content))
+    if lastest_message1 != lastest_message2:
+        if (int(latest_messages[0].content) - int(latest_messages[1].content)) == 1:
+            print("looking good")
+        else:   
+            print("Number squence break")
+            embed = discord.Embed(color = discord.Color.blue())
+            embed.add_field(name = f"**Breaking of chain :x: **", value = f":bangbang: The chain was broken by {str(latest_messages[0].author.mention)} at {str(latest_messages[1].content)} :bangbang:")
+            embed.set_thumbnail(url = f"{latest_messages[0].author.avatar_url}")
+            await channel.send(embed = embed)
+            await asyncio.sleep(20)
+            await clear(message,1000000000)
 
+    else:
+        embed = discord.Embed(color = discord.Color.blue())
+        embed.add_field(name = f"**Repeated User :x: **", value = f":bangbang: {lastest_message1.mention} Oh-oh dude what did you do, you just ruined the perfect going chain by sending your message twice :confused: :bangbang:")
+        await channel.send(embed=embed)
+        await asyncio.sleep(20)
+        await clear(message,10000000000)
+
+        # history = await channel.history().flatten()
+        # guild = message.guild
+        # if lastest_message1 == lastest_message2:
+        #     print("User repeated")
+        #     await clear(message,int(latest_messages[0].content))
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(color = discord.Color.blue())
+    embed.add_field(name = f"**__Welcome to counting bot__ :robot:**", value = f"Hello {ctx.author.name} :wave: Thanks for adding me to your server :blush: \n\nSo..basically i have only one command which is ``.set_counting_channel [channel]``. That channel becomes your counting channel for this server\n\n You will get all the information after you type the command\n\nGood luck :partying_face: and Happy Counting !!", inline = False)
+    await ctx.send(embed=embed)
 
 @client.command()
 async def clear(ctx, amount=5):
 	await ctx.channel.purge(limit=amount)
 
 
-client.run('TOKEN')
+client.run('TOKEN') 
